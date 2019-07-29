1ea3c8c0935aea1aa3d7ae4b3127c61676dc28ab
ravilella1234 <ravilella1234>
2019-07-29 07:18:30 +0530

    added second line

--- JulyProject/src/com/icici/loans/carloans/ThirdClass.java
+++ JulyProject/src/com/icici/loans/carloans/ThirdClass.java
@@ -5,7 +5,7 @@ public class ThirdClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("iam first line");
-		
+		System.out.println("iam second line");
 		
 	}
 
